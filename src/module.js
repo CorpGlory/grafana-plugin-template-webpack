32d3418291a9e7324a066ed23115c4232c772f75
rozetko <rozetko09@gmail.com>
2018-02-12 21:18:06 +0300

    Add CSS load to module.js

--- src/module.js
+++ src/module.js
@@ -1,5 +1,10 @@
 import { PanelCtrl } from 'grafana/app/plugins/sdk'; // will be resolved to app/plugins/sdk
 
+import './css/panel.base.scss';
+// Remove further imports if you don't need separate styles for light and dark themes
+import './css/panel.dark.scss';
+import './css/panel.light.scss';
+
 class Ctrl extends PanelCtrl {
 
   constructor($scope, $injector) {
@@ -7,11 +12,28 @@ class Ctrl extends PanelCtrl {
   }
 
   link(scope, element) {
-    
+    this.initStyles();
+  }
+
+  initStyles() {
+    window.System.import(this.panelPath + 'css/panel.base.css!');
+    // Remove further lines if you don't need separate styles for light and dark themes
+    if (grafanaBootData.user.lightTheme) {
+      window.System.import(this.panelPath + 'css/panel.light.css!');
+    } else {
+      window.System.import(this.panelPath + 'css/panel.dark.css!');
+    }
+  }
+
+  get panelPath() {
+    if (this._panelPath === undefined) {
+      this._panelPath = `/public/plugins/${this.pluginId}/`;
+    }
+    return this._panelPath;
   }
   
 }
 
-Ctrl.template = "<div>Hello from <b>Template Plugin</b></div>";
+Ctrl.templateUrl = 'partials/template.html';
 
 export { Ctrl as PanelCtrl }
