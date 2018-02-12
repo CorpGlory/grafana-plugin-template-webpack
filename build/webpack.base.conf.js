82640b27bd2870eca4a2f3f5fdf8ae4c679d268e
rozetko <rozetko09@gmail.com>
2018-02-12 21:17:43 +0300

    Update webpack config

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const ExtractTextPluginBase = new ExtractTextPlugin('./css/panel.base.css');
+const ExtractTextPluginLight = new ExtractTextPlugin('./css/panel.light.css');
+const ExtractTextPluginDark = new ExtractTextPlugin('./css/panel.dark.css');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  target: 'node',
+  context: resolve('src'),
+  entry: './module.js',
+  output: {
+    filename: "module.js",
+    path: resolve('dist'),
+    libraryTarget: "amd"
+  },
+  externals: [
+    // remove the line below if you don't want to use buildin versions
+    'jquery', 'lodash', 'moment',
+    function (context, request, callback) {
+      var prefix = 'grafana/';
+      if (request.indexOf(prefix) === 0) {
+        return callback(null, request.substr(prefix.length));
+      }
+      callback();
+    }
+  ],
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new CopyWebpackPlugin([
+      { from: 'plugin.json' },
+      { from: 'partials/*' }
+    ]),
+    ExtractTextPluginBase,
+    ExtractTextPluginLight,
+    ExtractTextPluginDark,
+  ],
+  resolve: {
+    alias: {
+      'src': resolve('src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(external)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.base\.(s?)css$/,
+        use: ExtractTextPluginBase.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.light\.(s?)css$/,
+        use: ExtractTextPluginLight.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.dark\.(s?)css$/,
+        use: ExtractTextPluginDark.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  }
+}
